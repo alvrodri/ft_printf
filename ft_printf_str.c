03b281db659b156155fd62df2ba548943687d922
Alvaro mariano Rodriguez benitez <alvrodri@c1r8s5.42madrid.com>
2020-02-26 11:33:08 +0100

    Recoded print_str

--- ft_printf_str.c
+++ ft_printf_str.c
@@ -3,81 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_str.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alvrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alvrodri <alvrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 17:24:47 by alvrodri          #+#    #+#             */
-/*   Updated: 2020/02/22 17:21:48 by alvrodri         ###   ########.fr       */
+/*   Updated: 2020/02/26 11:22:49 by alvrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_utils.h"
 
-void	ft_print_strprec(char *str, t_flags *flags)
-{
-	int i;
-
-	i = 0;
-	if (flags->minus == -1)
-		flags->written +=
-			ft_print_blank(ft_get_spaces(str, flags), flags->zero);
-	while (i < flags->precision && str[i])
-	{
-		write(1, &str[i], 1);
-		(flags->written)++;
-		i++;
-	}
-	if (flags->minus == 1)
-		flags->written +=
-			ft_print_blank(ft_get_spaces(str, flags), flags->zero);
-}
-
-void	ft_print_tstr(char *str, t_flags *flags, int i)
-{
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		(flags->written++);
-		i++;
-	}
-}
-
-int		ft_check_null(char **str, t_flags *flags)
-{
-	if (*str == NULL)
-		*str = ft_strdup("(null)");
-	else
-		*str = ft_strdup(*str);
-	if (flags->precision == 0)
-	{
-		flags->written +=
-			ft_print_blank(flags->width, flags->zero);
-		free(*str);
-		return (1);
-	}
-	return (0);
-}
-
 void	ft_print_str(va_list list, t_flags *flags)
 {
 	char	*str;
-	int		i;
+	char	*tmp;
+	int		len;
 
 	str = va_arg(list, char *);
-	if (ft_check_null(&str, flags) == 1)
-		return ;
-	if (flags->precision != -1)
+	if (str == NULL)
+		str = ft_strdup("(null)");
+	else
+		str = ft_strdup(str);
+	if (flags->precision > -1)
 	{
-		ft_print_strprec(str, flags);
-		free(str);
-		return ;
+		tmp = str;
+		str = ft_substr(tmp, 0, flags->precision);
+		free(tmp);
 	}
+	len = ft_strlen(str);
 	if (flags->width > 0 && flags->minus == -1)
-		flags->written +=
-			ft_print_blank(flags->width - ft_strlen(str), flags->zero);
-	i = 0;
-	ft_print_tstr(str, flags, i);
+		flags->written += ft_print_blank(flags->width - len, flags->zero);
+	flags->written += write(1, str, len);
 	if (flags->width > 0 && flags->minus == 1)
-		flags->written +=
-			ft_print_blank(flags->width - ft_strlen(str), flags->zero);
+		flags->written += ft_print_blank(flags->width - len, flags->zero);
 	free(str);
 }
